d0b9f7ed6d051c28ebfe1ac4a66fdb5a5902dab4
sarahmdht <sarahmdht@gmail.com>
2024-05-25 10:05:26 +0300

    header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './pages/search/search.component';
+import { BookingsComponent } from './pages/bookings/bookings.component';
+import { AdminComponent } from './pages/admin/admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent},
+    { path: 'search', component: SearchComponent},
+    { path: 'bookings', component: BookingsComponent},
+    { path: 'admin', component: AdminComponent}
+];
